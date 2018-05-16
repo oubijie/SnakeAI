44335ea842b07971ce7dbeb667096c43a7064587
夜猫 <575539640@qq.com>
2018-05-16 18:14:31 +0800

    增加了nodeState参数，用来标记蛇的转向结点特征

--- src/com/runningcode/tcs/Point.java
+++ src/com/runningcode/tcs/Point.java
@@ -10,6 +10,16 @@ public class Point {
 	private int x;
 	private int y;
 	
+	/**
+	 * @表明当前节点的状态
+	 * 借鉴数字小键盘
+	 * 7 8 9 
+	 * 4 5 6 
+	 * 1 2 3 
+	 * 有效参数为 7,9,1,3分别表示蛇主体转弯时，那个折点的状态
+	 */
+	private int nodeState;
+	
 
 	public Point(int x, int y) {
 		this.x = x;
@@ -20,6 +30,14 @@ public class Point {
 
 	}
 	
+	public int getNodeState() {
+		return nodeState;
+	}
+
+	public void setNodeState(int nodeState) {
+		this.nodeState = nodeState;
+	}
+
 	public int getX() {
 		return x;
 	}
